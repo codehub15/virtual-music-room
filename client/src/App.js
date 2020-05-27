dcc9582e5000c7583b89c209f3f838e452ff3131
natalialexandra <natalia.alexandra195@gmail.com>
2020-05-27 20:16:08 +0200

    set up client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import './style/App.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
