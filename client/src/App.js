d86b5c860b376373502808ca21b10291cf3f7a93
natalialexandra <natalia.alexandra195@gmail.com>
2020-05-28 16:29:46 +0200

    add navigation, routes, context and provider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import './style/App.scss';
+import Routes from './Routes'
+import Navbar from './components/Navbar'
+import AuthContext from './context/authContext'
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
+  const [token, setToken] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [cookies, setCookies] = useState(false)
+  const [name, setName] = useState(null)
+  const [userEmail, setUserEmail] = useState(null)
+  const [isAdmin, setIsAdmin] = useState(false)
 
-      </div>
-    </BrowserRouter>
+
+  return (
+    <AuthContext.Provider value={{
+      token, setToken,
+      isLoggedIn, setIsLoggedIn,
+      name, setName,
+      userEmail, setUserEmail,
+      isAdmin, setIsAdmin
+    }}
+    >
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+
+          <div className="app-container">
+            <Routes />
+          </div>
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
