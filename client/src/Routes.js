4caedc3c858cca62f36d5a53619d6f36313f6962
natalialexandra <natalia.alexandra195@gmail.com>
2020-05-28 16:27:49 +0200

    add client routes

---
+++ client/src/Routes.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import AuthContext from './context/authContext'
+import ErrorPage from './components/ErrorPage'
+import Home from './components/Homepage'
+// import Signup from './components/Signup'
+// import Login from './components/Login'
+// import Logout from './components/Logout'
+// import Musicians from './components/Musicians'
+// import MusicianAccount from './components/MusicianAccount'
+
+
+export default function Routes() {
+    const { isLoggedIn, setIsLoggedIn, isAdmin, setIsAdmin } = useContext(AuthContext)
+
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            {/** 
+
+             <Route path="/signup"
+                render={(props) =>
+                    <Signup
+                        {...props} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}
+                    />}
+            />
+
+
+            <Route path="/login"
+                render={(props) =>
+                    <Login
+                        {...props} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}
+                    />}
+            />
+
+
+            <Route exact path="/logout" component={Logout} />
+
+
+            <Route path="/musicianAccount"
+                render={(props) =>
+                    <MusicianAccount
+                        {...props}
+                        isLoggedIn={isLoggedIn}
+                        setIsLoggedIn={setIsLoggedIn}
+                    />}
+            />
+
+
+            <Route exact path="/musicians" component={Musicians} />
+
+            */}
+
+            <Route component={ErrorPage} />
+        </Switch>
+    )
+}
