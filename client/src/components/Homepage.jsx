a176d760fc85c11d836684a6feccd679a64f909f
natalialexandra <natalia.alexandra195@gmail.com>
2020-05-28 18:51:57 +0200

    add slogan

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -5,6 +5,10 @@ import AudioRoom from './AudioRoom'
 function Homepage() {
     return (
         <div className="home-component">
+            <div className="slogan">
+                <h1>Virtual Music Room</h1>
+                <h2>The Musicians Community</h2>
+            </div>
             <div className="particles-map">
                 <TextParticles />
             </div>
