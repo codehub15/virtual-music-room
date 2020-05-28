0a2a263264e944c2576ea5b934ed4f16e203a123
JuanSessions <juanperusin@yahoo.es>
2020-05-28 17:05:41 +0200

    add audio-room and particle components to homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import TextParticles from './TextParticles'
+import AudioRoom from './AudioRoom'
 
 function Homepage() {
     return (
         <div className="home-component">
-
             <div className="particles-map">
                 <TextParticles />
             </div>
+            <AudioRoom />
         </div>
     )
 }
 
 export default Homepage
-
