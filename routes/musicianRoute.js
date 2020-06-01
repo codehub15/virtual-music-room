bbd9ff311daeaa83f3a971c8a74320ddf7f6c4f3
natalialexandra <natalia.alexandra195@gmail.com>
2020-06-01 18:23:29 +0200

    add route for musicians

---
+++ routes/musicianRoute.js
@@ -0,0 +1,16 @@
+const Route = require("express").Router()
+const { getUsers, getUser, postUser, putUser, deleteUser, login } = require("../controllers/musicianController")
+const { validateInputs } = require("../middleware/validator")
+const auth = require("../middleware/authenticator")
+const isAdmin = require("../middleware/rolesAuth")
+
+Route.post("/login", login)
+Route.get("/", getUsers)
+// Route.get("/:id", getUser)
+// Route.get("/", auth, isAdmin, getUsers)
+Route.get("/:id", auth, getUser)
+Route.post("/", validateInputs(), postUser)
+Route.put("/:id", auth, putUser)
+Route.delete("/:id", auth, deleteUser)
+
+module.exports = Route
