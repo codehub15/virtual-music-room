bc872f8aa912c917a30d4246a6a8085a2d8c58b1
JuanSessions <juanperusin@yahoo.es>
2020-06-29 20:22:27 +0200

    changes for heroku

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 exports.indexController = (req, res, next) => {
-    res.send("received get request")
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
 }
