902034ae5e24b78fbd15fa5f903474a352e1dfef
JuanSessions <juanperusin@yahoo.es>
2020-06-30 13:46:49 +0200

    backend-deploy

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 exports.indexController = (req, res, next) => {
+    console.log("index path:", (__dirname, "../client", "build", "index.html"))
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
 }
