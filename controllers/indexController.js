3d6b38f80f81dfff117c2ff02daf6452c23e4058
JuanSessions <juanperusin@yahoo.es>
2020-06-30 15:29:27 +0200

    small changes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,5 @@
 const path = require("path");
 
 exports.indexController = (req, res, next) => {
-    console.log("index path:", (__dirname, "../client", "build", "index.html"))
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
 }
