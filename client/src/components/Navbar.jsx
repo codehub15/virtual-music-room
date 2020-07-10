7aba98583f01489b29624e40c56176ccb2fd2390
natalialexandra <natalia.alexandra195@gmail.com>
2020-07-10 17:58:42 +0200

    responive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import AuthContext from '../context/authContext'
 
 
 export default function Navbar() {
     const { isLoggedIn } = useContext(AuthContext)
+    const [navbarToggle, setNavbarToggle] = useState(false)
 
     return (
-        <nav>
-            <ul>
-                <li className="link-background">
-                    <NavLink className="main-nav-link middle" to="/" exact activeClassName="active">VMR Home</NavLink>
+        <nav className="navbar">
+            <span class="navbar-toggle-icon" id="js-navbar-toggle" onClick={() => { setNavbarToggle((navbarToggle) => !navbarToggle) }}>
+                <i class="fas fa-bars"></i>
+            </span>
+            <NavLink className="main-nav-link middle logo" to="/" exact activeClassName="active">VMR Home</NavLink>
+            <ul className={`main-nav ${navbarToggle ? "toggle" : ""}`} >
+                {/**   
+            <li className="link-background">
+                    <NavLink className="main-nav-link middle logo" to="/" exact activeClassName="active">VMR Home</NavLink>
                 </li>
+                 */}
                 {
                     isLoggedIn && (
                         <>
